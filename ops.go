10614f2f6f7c6dbb5544edea69186175036887c3
Mariusz Gronczewski <xani666@gmail.com>
2016-11-12 19:49:02 +0100

    fix typo, more error handling

--- ops.go
+++ ops.go
@@ -2,6 +2,7 @@ package gitcli
 
 import (
 	"strings"
+	"fmt"
 )
 
 func (r *Repo)Push(extraArgs ...string) error {
@@ -16,7 +17,7 @@ func (r *Repo)Pull(extraArgs ...string) error {
 }
 
 func (r *Repo)Fetch(extraArgs ...string) error {
-	args := []string{`push`}
+	args := []string{`fetch`}
 	return r.commonCmd(args, extraArgs...)
 }
 
@@ -27,10 +28,12 @@ func (r *Repo)Checkout(extraArgs ...string) error {
 
 // Add or set a given remote
 func (r *Repo)SetRemote(remoteName string, remoteUrl string ) error {
-	_, stderr, err := r.cmd("remote","set-url",remoteName,remoteUrl)
+	stdout, stderr, err := r.cmd("remote","set-url",remoteName,remoteUrl)
 	if strings.Contains(stderr,"No such remote") {
 		err = r.commonCmd([]string{"remote","add"},remoteName,remoteUrl)
 		return err
+	} else if err != nil {
+		return fmt.Errorf("set remote failed with stdout:%s , stderr: %s", stdout, stderr)
 	}
 	return err
 }
