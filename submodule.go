33e140265985dc279e32662400cfa0f72b707702
Mariusz Gronczewski <xani666@gmail.com>
2016-12-11 16:15:31 +0100

    add submodule sync

--- submodule.go
+++ submodule.go
@@ -10,6 +10,11 @@ func (r *Repo)SubmoduleInit(extraArgs ...string) error {
 	return r.commonCmd(args, extraArgs...)
 }
 
+func (r *Repo)SubmoduleSync(extraArgs ...string) error {
+	args := []string{`submodule`,`sync`}
+	return r.commonCmd(args, extraArgs...)
+}
+
 func (r *Repo)SubmoduleUpdate(extraArgs ...string) error {
 	args := []string{`submodule`,`update`}
 	return r.commonCmd(args, extraArgs...)
